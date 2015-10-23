3af884f2092b9a486614534dbb7eeb1b55fbf664
Javier Lozano <javier@lozanotek.com>
2015-10-23 09:51:27 -0500

    Adding the Middleware demo

---
+++ demos/Middleware/src/LoggerMiddleware/LoggerMiddlewareExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Builder;
+
+namespace LoggerMiddleware
+{
+    // Source: https://docs.asp.net/en/latest/fundamentals/middleware.html
+    public static class LoggerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestLogger(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestLogger>();
+        }
+    }
+}
---
+++ demos/Middleware/src/LoggerMiddleware/RequestLogger.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNet.Builder;
+using Microsoft.Framework.Logging;
+using Microsoft.AspNet.Http;
+
+namespace LoggerMiddleware
+{
+    // Source: https://docs.asp.net/en/latest/fundamentals/middleware.html
+    public class RequestLogger
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public RequestLogger(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<RequestLogger>();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _logger.LogInformation($"Request: {context.Request.Path}");
+            context.Response.OnResponseCompleted(obj =>
+            {
+                var l = obj as ILogger;
+                l.LogInformation("Response Completed");
+            }, _logger);
+
+            await _next.Invoke(context);
+            _logger.LogInformation("Completed Logging Request");
+        }
+    }
+}
